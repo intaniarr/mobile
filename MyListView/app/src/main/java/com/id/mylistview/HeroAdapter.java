1364f14a3cfd52aecfbcba3f28eac09b7172f990
intaniarr <rahmadhillaintan@gmail.com>
2020-10-23 22:02:57 +0700

    My List View

--- MyListView/app/src/main/java/com/id/mylistview/HeroAdapter.java
+++ MyListView/app/src/main/java/com/id/mylistview/HeroAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HeroAdapter extends BaseAdapter {
 
     private final Context context;
