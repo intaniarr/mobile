52ce181ace402deed617423b14baa20661728697
intaniarr <rahmadhillaintan@gmail.com>
2020-11-27 20:28:15 +0700

    Sms Listener App

---
+++ SmsListenerApp/app/src/main/java/com/id/smslistenerapp/SmsReceiverActivity.java
@@ -0,0 +1,40 @@
+package com.id.smslistenerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SmsReceiverActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvSmsFrom;
+    private TextView tvSmsMessage;
+    private Button btnClose;
+    public static final String EXTRA_SMS_NO = "extra_sms_no";
+    public static final String EXTRA_SMS_MESSAGE = "extra_sms_message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_receiver);
+
+        tvSmsFrom = findViewById(R.id.tv_no);
+        tvSmsMessage = findViewById(R.id.tv_message);
+        btnClose = findViewById(R.id.btn_close);
+
+        String senderNo = getIntent().getStringExtra(EXTRA_SMS_NO);
+        String senderMessage = getIntent().getStringExtra(EXTRA_SMS_MESSAGE);
+        tvSmsFrom.setText("from :"+senderNo);
+        tvSmsMessage.setText(senderMessage);
+
+        btnClose.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_close) {
+            finish();
+        }
+    }
+}
